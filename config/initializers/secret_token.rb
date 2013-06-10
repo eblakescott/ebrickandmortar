3f3d22ca7bc59820083c0e4217aaad62cf144ef9
Ed Scott <eblakescott@gmail.com>
2013-06-10 07:04:14 -0500

    initial commit of new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ebrickandmortar::Application.config.secret_key_base = 'faafd067951c1db50a73ef4084555c4ac6c4b9ac0a293b50b2c4edbcc359733ac9a386490a07d7b82371608ee8a99b31139cf36ac0bf099d149c5eaad52560f1'
