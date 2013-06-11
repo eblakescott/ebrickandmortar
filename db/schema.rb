ebd02b72afeed36cc39897a538d2fcd3301c2b3f
Ed Scott <eblakescott@gmail.com>
2013-06-11 06:40:20 -0500

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130610181744) do
+ActiveRecord::Schema.define(version: 20130611113855) do
+
+  create_table "pages", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "stores", force: true do |t|
     t.string   "title"
