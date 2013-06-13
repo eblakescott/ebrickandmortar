0e9385796fceeaa4e7327052934be586ed908fcf
Ed Scott <eblakescott@gmail.com>
2013-06-13 08:00:55 -0500

    generated scaffold for Product; migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130613124901) do
+ActiveRecord::Schema.define(version: 20130613125830) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20130613124901) do
   create_table "pages", force: true do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
