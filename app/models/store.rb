b74147d7d9b023b931e716c26446e1e815ad6957
Ed Scott <eblakescott@gmail.com>
2013-06-14 10:40:20 -0500

    changed links in application layouts; added search functionality (doesn't work for SQLite, should work when deployed to Heroku

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
 has_many :products, dependent: :destroy
+
+def self.search(search)
+  if search
+    where('title ILIKE ? OR description ILIKE ? OR address ILIKE ?', "%#{search}%", "%#{search}%")
+  else
+    scoped
+  end
+end
 end
