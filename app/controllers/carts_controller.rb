64be2776e83089080a236139937c86ae936835a2
Ed Scott <eblakescott@gmail.com>
2013-06-27 19:27:30 -0500

    completed redirect of invalid cart to homepage

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -55,7 +55,8 @@ class CartsController < ApplicationController
   # DELETE /carts/1.json
   def destroy
     @cart.destroy
-    respond_to do |format|
+      session[:cart_id] = nil 
+      respond_to do |format|
       format.html { redirect_to carts_url }
       format.json { head :no_content }
     end
@@ -70,5 +71,9 @@ class CartsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
+    end
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to root_url, notice: 'Invalid cart'
     end
 end
